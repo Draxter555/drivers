7a86a96af5c114f29b8db3b996dd6ce4bcb07738
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-14 20:43:09 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void) {
+    char buffer[100];
+    ssize_t bytes_read;
+
+    int fd = open("/dev/zero", O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return EXIT_FAILURE;
+    }
+
+    bytes_read = read(fd, buffer, sizeof(buffer));
+    if (bytes_read < 0) {
+        perror("read");
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    close(fd);
+    return EXIT_SUCCESS;
+}
