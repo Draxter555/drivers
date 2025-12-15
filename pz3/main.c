3b85ee73152c4df9b00291bae42046c94c1615d8
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-15 15:07:37 +0300

    Update main.c

--- pz3/main.c
+++ pz3/main.c
@@ -3,13 +3,13 @@
 
 int major;
 
-static int simple_open(struct inode *inode, struct file *file)
+int simple_open(struct inode *inode, struct file *file)
 {
     printk("Device opened\n");
     return 0;
 }
 
-static int simple_release(struct inode *inode, struct file *file)
+int simple_release(struct inode *inode, struct file *file)
 {
     printk("Device closed\n");
     return 0;
@@ -22,14 +22,14 @@ struct file_operations fops = {
 
 int init_module(void)
 {
-    major = register_chrdev(0, "simple_char", &fops);
+    major = register_chrdev(0, "main", &fops);
     printk("Driver loaded, major=%d\n", major);
     return 0;
 }
 
 void cleanup_module(void)
 {
-    unregister_chrdev(major, "simple_char");
+    unregister_chrdev(major, "main");
     printk("Driver removed\n");
 }
 
