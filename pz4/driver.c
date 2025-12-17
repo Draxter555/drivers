e227bd4bd921c27b0c6b62617703a37a80142745
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-17 21:29:12 +0300

    Update driver.c

--- pz4/driver.c
+++ pz4/driver.c
@@ -2,129 +2,91 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
-#include <linux/ioctl.h>
-#include <linux/mutex.h>
-#include <linux/slab.h>
 #include <linux/device.h>
+#include <linux/mutex.h>
+#include <linux/wait.h>
 
 #define DEVICE_NAME "pz4_dev"
 #define BUF_SIZE 256
+#define IOCTL_CLEAR _IO('q',1)
+#define IOCTL_HASDATA _IOR('q',2,int)
 
-#define IOCTL_CLEAR _IO('q', 1)
-#define IOCTL_HASDATA _IOR('q', 2, int)
-
-static char *buffer;
+static char buf[BUF_SIZE];
 static int buf_len = 0;
-static struct cdev c_dev;
+
 static dev_t dev;
-static struct class *my_class;
+static struct cdev c_dev;
+static struct class *cls;
 static DEFINE_MUTEX(buf_mutex);
+static DECLARE_WAIT_QUEUE_HEAD(read_queue);
 
-static int pz4_open(struct inode *inode, struct file *file) { return 0; }
-static int pz4_release(struct inode *inode, struct file *file) { return 0; }
+static int open_f(struct inode *i, struct file *f) { return 0; }
+static int release_f(struct inode *i, struct file *f) { return 0; }
 
-static ssize_t pz4_read(struct file *file, char __user *user_buf, size_t len, loff_t *offset)
+static ssize_t read_f(struct file *f, char __user *u, size_t l, loff_t *off)
 {
-    ssize_t ret = 0;
-    if (mutex_lock_interruptible(&buf_mutex)) return -ERESTARTSYS;
-    if (*offset >= buf_len) goto out;
-    if (len > buf_len - *offset) len = buf_len - *offset;
-    if (copy_to_user(user_buf, buffer + *offset, len)) ret = -EFAULT;
-    else { *offset += len; ret = len; }
-out:
+    if (wait_event_interruptible(read_queue, buf_len>0)) return -ERESTARTSYS;
+    mutex_lock(&buf_mutex);
+    if (l>buf_len) l=buf_len;
+    if (copy_to_user(u, buf, l)) l=-EFAULT;
+    else buf_len=0;
     mutex_unlock(&buf_mutex);
-    return ret;
+    return l;
 }
 
-static ssize_t pz4_write(struct file *file, const char __user *user_buf, size_t len, loff_t *offset)
+static ssize_t write_f(struct file *f, const char __user *u, size_t l, loff_t *off)
 {
-    ssize_t ret = 0;
-    if (len > BUF_SIZE) len = BUF_SIZE;
-    if (mutex_lock_interruptible(&buf_mutex)) return -ERESTARTSYS;
-    if (copy_from_user(buffer, user_buf, len)) ret = -EFAULT;
-    else { buf_len = len; ret = len; }
+    if (l>BUF_SIZE) l=BUF_SIZE;
+    mutex_lock(&buf_mutex);
+    if (copy_from_user(buf, u, l)) l=-EFAULT;
+    else buf_len=l;
     mutex_unlock(&buf_mutex);
-    return ret;
+    wake_up_interruptible(&read_queue);
+    return l;
 }
 
-static long pz4_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+static long ioctl_f(struct file *f, unsigned int cmd, unsigned long arg)
 {
     int tmp;
-    if (mutex_lock_interruptible(&buf_mutex)) return -ERESTARTSYS;
-    switch(cmd) {
-        case IOCTL_CLEAR:
-            buf_len = 0;
-            break;
-        case IOCTL_HASDATA:
-            tmp = (buf_len != 0);
-            if (copy_to_user((int __user *)arg, &tmp, sizeof(int))) {
-                mutex_unlock(&buf_mutex);
-                return -EFAULT;
-            }
-            break;
-        default:
-            mutex_unlock(&buf_mutex);
-            return -EINVAL;
+    mutex_lock(&buf_mutex);
+    if (cmd==IOCTL_CLEAR) buf_len=0;
+    else if (cmd==IOCTL_HASDATA){
+        tmp=(buf_len!=0);
+        copy_to_user((int __user*)arg,&tmp,sizeof(int));
     }
+    else { mutex_unlock(&buf_mutex); return -EINVAL; }
     mutex_unlock(&buf_mutex);
     return 0;
 }
 
-static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = pz4_open,
-    .release = pz4_release,
-    .read = pz4_read,
-    .write = pz4_write,
-    .unlocked_ioctl = pz4_ioctl,
+static struct file_operations fops={
+    .owner=THIS_MODULE,
+    .open=open_f,
+    .release=release_f,
+    .read=read_f,
+    .write=write_f,
+    .unlocked_ioctl=ioctl_f,
 };
 
-static int __init pz4_init(void)
+static int __init init_f(void)
 {
-    buffer = kmalloc(BUF_SIZE, GFP_KERNEL);
-    if (!buffer) return -ENOMEM;
-
-    if (alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME) < 0) {
-        kfree(buffer);
-        return -1;
-    }
-
-    cdev_init(&c_dev, &fops);
-    if (cdev_add(&c_dev, dev, 1) < 0) {
-        unregister_chrdev_region(dev, 1);
-        kfree(buffer);
-        return -1;
-    }
-
-    my_class = class_create(THIS_MODULE, "pz4_class");
-    if (IS_ERR(my_class)) {
-        cdev_del(&c_dev);
-        unregister_chrdev_region(dev, 1);
-        kfree(buffer);
-        return PTR_ERR(my_class);
-    }
-
-    device_create(my_class, NULL, dev, NULL, DEVICE_NAME);
-
+    alloc_chrdev_region(&dev,0,1,DEVICE_NAME);
+    cdev_init(&c_dev,&fops);
+    cdev_add(&c_dev,dev,1);
+    cls=class_create(THIS_MODULE,"pz4_class");
+    device_create(cls,NULL,dev,NULL,DEVICE_NAME);
     mutex_init(&buf_mutex);
-    printk(KERN_INFO "PZ4 driver loaded\n");
     return 0;
 }
 
-static void __exit pz4_exit(void)
+static void __exit exit_f(void)
 {
-    device_destroy(my_class, dev);
-    class_destroy(my_class);
+    device_destroy(cls,dev);
+    class_destroy(cls);
     cdev_del(&c_dev);
-    unregister_chrdev_region(dev, 1);
-    kfree(buffer);
-    printk(KERN_INFO "PZ4 driver removed\n");
+    unregister_chrdev_region(dev,1);
 }
 
-module_init(pz4_init);
-module_exit(pz4_exit);
-
+module_init(init_f);
+module_exit(exit_f);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Ваше Имя");
-MODULE_DESCRIPTION("PZ4 character device driver");
-MODULE_VERSION("1.0");
