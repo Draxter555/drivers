bce4856530d8c501199cf6d311ade58927498ea0
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-18 21:55:41 +0300

    Update app.c

--- pz5/app.c
+++ pz5/app.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <unistd.h>
 
+int main() {
+    unsigned char buf[24];
+    int fd = open("/dev/rawdemo", O_RDONLY);
+    if(fd < 0) { perror("open"); return 1; }
+    read(fd, buf, sizeof(buf));
+    close(fd);
 
-int main()
-{
-printf("Lab5 app started\n");
-printf("Check dmesg and ip link\n");
-sleep(1);
-return 0;
+    printf("RAW HEX:");
+    for(int i=0;i<24;i++)
+        printf(" %02x", buf[i]);
+    printf("\nASCII:");
+    for(int i=0;i<24;i++)
+        if(buf[i]>=32 && buf[i]<=126)
+            printf(" %c", buf[i]);
+        else
+            printf(" \\x%02x", buf[i]);
+    printf("\n");
+    return 0;
 }
