4087cc5f70217de7df6114039e750639ba661f82
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-19 21:03:19 +0300

    Update main.c

--- pz5/main.c
+++ pz5/main.c
@@ -2,65 +2,65 @@
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
 
-#define DRV_NAME "lab6_net"
+#define DRV_NAME "lab5_net"
 
-static int lab6_open(struct net_device *dev)
+static int lab5_open(struct net_device *dev)
 {
-    pr_info("lab6: device opened\n");
+    pr_info("lab5: device opened\n");
     netif_start_queue(dev);
     return 0;
 }
 
-static int lab6_stop(struct net_device *dev)
+static int lab5_stop(struct net_device *dev)
 {
-    pr_info("lab6: device stopped\n");
+    pr_info("lab5: device stopped\n");
     netif_stop_queue(dev);
     return 0;
 }
 
-static netdev_tx_t lab6_xmit(struct sk_buff *skb, struct net_device *dev)
+static netdev_tx_t lab5_xmit(struct sk_buff *skb, struct net_device *dev)
 {
-    pr_info("lab6: packet received, len=%u\n", skb->len);
+    pr_info("lab5: packet received, len=%u\n", skb->len);
     dev_kfree_skb(skb);   // имитация отправки
     return NETDEV_TX_OK;
 }
 
-static const struct net_device_ops lab6_ops = {
-    .ndo_open       = lab6_open,
-    .ndo_stop       = lab6_stop,
-    .ndo_start_xmit = lab6_xmit,
+static const struct net_device_ops lab5_ops = {
+    .ndo_open       = lab5_open,
+    .ndo_stop       = lab5_stop,
+    .ndo_start_xmit = lab5_xmit,
 };
 
-static void lab6_setup(struct net_device *dev)
+static void lab5_setup(struct net_device *dev)
 {
     ether_setup(dev);
-    dev->netdev_ops = &lab6_ops;
+    dev->netdev_ops = &lab5_ops;
     eth_random_addr(dev->dev_addr);
 }
 
-static struct net_device *lab6_dev;
+static struct net_device *lab5_dev;
 
-static int __init lab6_init(void)
+static int __init lab5_init(void)
 {
-    pr_info("lab6: init\n");
+    pr_info("lab5: init\n");
 
-    lab6_dev = alloc_netdev(0, "lab6%d", NET_NAME_UNKNOWN, lab6_setup);
-    if (!lab6_dev)
+    lab5_dev = alloc_netdev(0, "lab5%d", NET_NAME_UNKNOWN, lab5_setup);
+    if (!lab5_dev)
         return -ENOMEM;
 
-    return register_netdev(lab6_dev);
+    return register_netdev(lab5_dev);
 }
 
-static void __exit lab6_exit(void)
+static void __exit lab5_exit(void)
 {
-    pr_info("lab6: exit\n");
-    unregister_netdev(lab6_dev);
-    free_netdev(lab6_dev);
+    pr_info("lab5: exit\n");
+    unregister_netdev(lab5_dev);
+    free_netdev(lab5_dev);
 }
 
-module_init(lab6_init);
-module_exit(lab6_exit);
+module_init(lab5_init);
+module_exit(lab5_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("student");
-MODULE_DESCRIPTION("Lab6 virtual network driver");
+MODULE_DESCRIPTION("Lab5 virtual network driver");
