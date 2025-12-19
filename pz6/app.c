c0014f8d83349a3aaed5bfd13dd64cc08bdf44ef
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-19 22:11:33 +0300

    Create app.c

---
+++ pz6/app.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <linux/sockios.h>
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct ifreq ifr;
+
+    // Создаём сокет для работы с сетевыми интерфейсами
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    // Указываем имя интерфейса
+    strcpy(ifr.ifr_name, "my_pci_net");
+
+    // Получаем MAC-адрес
+    if (ioctl(sock, SIOCGIFHWADDR, &ifr) < 0) {
+        perror("ioctl SIOCGIFHWADDR");
+        close(sock);
+        exit(1);
+    }
+
+    printf("MAC-адрес интерфейса %s: ", ifr.ifr_name);
+    for (int i = 0; i < 6; i++) {
+        printf("%02X%s", (unsigned char)ifr.ifr_hwaddr.sa_data[i],
+               (i < 5) ? ":" : "\n");
+    }
+
+    close(sock);
+    return 0;
+}
