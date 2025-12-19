597843fe737ac18a274f402a8afc08c85d056a70
Draxter555 <50377634+Draxter555@users.noreply.github.com>
2025-12-20 00:45:56 +0300

    Update ioctl.h

--- lab1/ioctl.h
+++ lab1/ioctl.h
@@ -3,6 +3,9 @@
 
 #include <linux/ioctl.h>
 
-#define IOCTL_HISTO _IOR('k', 0, int)
+#define HISTO_MAX 500
+
+#define IOCTL_HISTO_LEN _IOR('k', 0, size_t)
+#define IOCTL_HISTO_BUF _IOR('k', 1, size_t[HISTO_MAX])
 
 #endif
